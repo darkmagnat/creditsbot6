cabb7d8f296dcceb60bbdb1bde3ad3af66b3c6f3
darkmagnat <43116040+darkmagnat@users.noreply.github.com>
2018-11-15 17:25:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,4 +14,27 @@ var channel = "508667278137753600";//ايدي الروم
 })
 
 
+const adminprefix = "1";
+const devs = ['383711936174620672','481231486104764427'];
+client.on('message', message => {
+    client.on('message', message => {
+        var prefix = "1";
+        if (message.author.bot) return;
+        if (!message.content.startsWith(prefix)) return;
+      
+        let command = message.content.split(" ")[0];
+        command = command.slice(prefix.length);
+      
+      
+      let args = message.content.split(" ").slice(1);
+      let x = args.join(" ")
+        if(message.content.startsWith(prefix + 'ألامر')) {
+            message.channel.send(''+x);
+                message.delete(999)
+        }
+        
+       
+      });
+
+
 client.login(process.env.BOT_TOKEN);
