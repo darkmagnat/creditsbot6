b344ee0be1a9c94847ae6b0318baceaf7d6c8714
darkmagnat <43116040+darkmagnat@users.noreply.github.com>
2018-11-15 18:22:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,20 +13,6 @@ var channel = "508667278137753600";//ايدي الروم
     },305);
 })
 
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
 
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "say") {
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|`ADMINISTRATOR`ليس لديك صلاحيات`');
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
 
 client.login(process.env.BOT_TOKEN);
